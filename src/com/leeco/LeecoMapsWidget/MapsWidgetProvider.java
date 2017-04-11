007d6233548c23cc36f3049ed2137566c0274fb2
Ding Li <dingli@le.com>
2017-04-11 18:37:10 +0800

    LeccoMapsWidget: Add Google Map Widget Placeholder Proto
    Add Google Map Widget Placeholder Proto
    
    Ticket: IOFV-5567
    
    Change-Id: I1288e92f96d726a0e2f66341413a2f9521164e74
    Signed-off-by: Ding Li <dingli@le.com>

--- src/com/leeco/LeecoMapsWidget/MapsWidgetProvider.java
+++ src/com/leeco/LeecoMapsWidget/MapsWidgetProvider.java
@@ -1,5 +1,6 @@
 package com.leeco.LeecoMapsWidget;
 
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -7,22 +8,20 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
-
 /**
  * Created by root on 17-3-23.
  */
-public class MapsWidgetProvider extends AppWidgetProvider {
+public class MapsWidgetProvider extends AppWidgetProvider  {
     private static final String TAG = "MapsWidgetProvider";
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         defaultAppWidget(context, appWidgetIds);
-
     }
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-
+        receive(context,intent);
 
     }
     private void defaultAppWidget(Context context, int[] appWidgetIds) {
@@ -46,6 +45,25 @@ public class MapsWidgetProvider extends AppWidgetProvider {
         intent.setComponent(new ComponentName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity"));
         pendingIntent = PendingIntent.getActivity(context,
                 0 /* no requestCode */, intent, 0 /* no flags */);
-        views.setOnClickPendingIntent(R.id.widget_main, pendingIntent);
+        views.setOnClickPendingIntent(R.id.maps_google, pendingIntent);
+    }
+
+    private void receive(Context context, Intent intent){
+        String action = intent.getAction();
+        String title = intent.getStringExtra(Notification.EXTRA_TITLE);
+        String text = intent.getStringExtra(Notification.EXTRA_TEXT);
+        String icon = intent.getStringExtra(Notification.EXTRA_SMALL_ICON);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        final RemoteViews rvs = new RemoteViews(context.getPackageName(), R.layout.maps_appwidget);
+        if (action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
+            if(title != null){
+                rvs.setTextViewText(R.id.maps_title, title);
+                rvs.setTextViewText(R.id.maps_text, text);
+                rvs.setImageViewResource(R.id.maps_icon, R.drawable.ic_launcher);
+                appWidgetManager.updateAppWidget(new ComponentName(context, MapsWidgetProvider.class), rvs);
+            }
+
+        }
+
     }
 }
