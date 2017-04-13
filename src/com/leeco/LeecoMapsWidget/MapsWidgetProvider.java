ee4437bfc83ba36be12f42efbc28a29a20c3f4d8
Ding Li <dingli@le.com>
2017-04-13 17:03:11 +0800

    LeccoMapsWidget: Changing the way to get data
    Changing the way to get data,get data from NotificationManagerService
    
    Ticket: IOFV-5567
    
    Change-Id: Ifa85297852ff2d3e3b5200cb713253df83d33d39
    Signed-off-by: Ding Li <dingli@le.com>

--- src/com/leeco/LeecoMapsWidget/MapsWidgetProvider.java
+++ src/com/leeco/LeecoMapsWidget/MapsWidgetProvider.java
@@ -1,34 +1,48 @@
 package com.leeco.LeecoMapsWidget;
 
 import android.app.Notification;
+import android.app.IInterceptNotification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.os.RemoteException;
+import android.service.notification.StatusBarNotification;
+import android.view.View;
 import android.widget.RemoteViews;
+
 /**
- * Created by root on 17-3-23.
+ * Created by dingli on 17-3-23.
  */
-public class MapsWidgetProvider extends AppWidgetProvider  {
+public class MapsWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "MapsWidgetProvider";
+    private Context mContext;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         defaultAppWidget(context, appWidgetIds);
+        final NotificationManager nmr = context.getSystemService(NotificationManager.class);
+        nmr.registerNotificationInterceptListener(mNotificationListener);
+        mContext = context;
     }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
-        receive(context,intent);
-
+//        receive(context,intent);
     }
+
     private void defaultAppWidget(Context context, int[] appWidgetIds) {
         final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.maps_appwidget);
         linkButtons(context, views, false /* not playing */);
         pushUpdate(context, appWidgetIds, views);
     }
+
     private void pushUpdate(Context context, int[] appWidgetIds, RemoteViews views) {
         final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
         if (appWidgetIds != null) {
@@ -37,6 +51,7 @@ public class MapsWidgetProvider extends AppWidgetProvider  {
             appWidgetManager.updateAppWidget(new ComponentName(context, this.getClass()), views);
         }
     }
+
     private void linkButtons(Context context, RemoteViews views, boolean playerActive) {
         Intent intent;
         PendingIntent pendingIntent;
@@ -46,24 +61,86 @@ public class MapsWidgetProvider extends AppWidgetProvider  {
         pendingIntent = PendingIntent.getActivity(context,
                 0 /* no requestCode */, intent, 0 /* no flags */);
         views.setOnClickPendingIntent(R.id.maps_google, pendingIntent);
+        views.setViewVisibility(R.id.maps_background, View.GONE);
+        views.setViewVisibility(R.id.maps_linear, View.VISIBLE);
+        views.setViewVisibility(R.id.maps_icon, View.VISIBLE);
     }
 
-    private void receive(Context context, Intent intent){
+    private void receive(Context context, Intent intent) {
         String action = intent.getAction();
-        String title = intent.getStringExtra(Notification.EXTRA_TITLE);
-        String text = intent.getStringExtra(Notification.EXTRA_TEXT);
-        String icon = intent.getStringExtra(Notification.EXTRA_SMALL_ICON);
+        StatusBarNotification notification = intent.getParcelableExtra("n");
+        if (notification == null) {
+            return;
+        }
+        String pkg = notification.getPackageName();
+        if (pkg.equals("com.leeco.notificationTestData") || pkg.equals("com.google.android.apps.maps")) {
+
+            Notification n = notification.getNotification();
+            if (n == null) {
+                return;
+            }
+
+            fillReceiveData(context, n, action);
+        }
+
+    }
+
+    private void fillReceiveData(Context context, Notification n, String action) {
+        String title = n.extras.getString(Notification.EXTRA_TITLE);
+        String text = n.extras.getString(Notification.EXTRA_TEXT);
+        Icon iconSmall = n.getSmallIcon();
         AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        final RemoteViews rvs = new RemoteViews(context.getPackageName(), R.layout.maps_appwidget);
+        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.maps_appwidget);
+
         if (action.equals(AppWidgetManager.ACTION_APPWIDGET_UPDATE)) {
-            if(title != null){
-                rvs.setTextViewText(R.id.maps_title, title);
-                rvs.setTextViewText(R.id.maps_text, text);
-                rvs.setImageViewResource(R.id.maps_icon, R.drawable.ic_launcher);
-                appWidgetManager.updateAppWidget(new ComponentName(context, MapsWidgetProvider.class), rvs);
+            if (title != null) {
+                views.setTextViewText(R.id.maps_title, title);
+                views.setTextViewText(R.id.maps_text, text);
+                views.setImageViewIcon(R.id.maps_icon, iconSmall);
+                appWidgetManager.updateAppWidget(new ComponentName(context, MapsWidgetProvider.class), views);
             }
 
         }
+    }
 
+    /**
+     * Implementation of {@link android.app.IInterceptNotification} to listen notification changes from
+     * NotificationManagerService.
+     * sendNotificationCallback(in Notification notification, String title, String text);
+     */
+    private IInterceptNotification.Stub mNotificationListener = new IInterceptNotification.Stub() {
+        @Override
+        public void sendNotificationCallback(StatusBarNotification notification) throws RemoteException {
+            if (notification == null) {
+                return;
+            }
+            String pkg = notification.getPackageName();
+
+            if (pkg.equals("com.leeco.notificationTestData") || pkg.equals("com.google.android.apps.maps")) {
+                final Notification n = notification.getNotification();
+                if (n == null) {
+                    return;
+                }
+                fillWidgetData(n);
+            }
+
+        }
+    };
+
+    private void fillWidgetData(Notification n) {
+        String title = n.extras.getString(Notification.EXTRA_TITLE);
+        String text = n.extras.getString(Notification.EXTRA_TEXT);
+        Icon iconSmall = n.getSmallIcon();
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
+        final RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.maps_appwidget);
+        views.setViewVisibility(R.id.maps_background, View.GONE);
+        views.setViewVisibility(R.id.maps_linear, View.VISIBLE);
+        views.setViewVisibility(R.id.maps_icon, View.VISIBLE);
+        if (title != null) {
+            views.setTextViewText(R.id.maps_title, title);
+            views.setTextViewText(R.id.maps_text, text);
+            views.setImageViewIcon(R.id.maps_icon, iconSmall);
+            appWidgetManager.updateAppWidget(new ComponentName(mContext, MapsWidgetProvider.class), views);
+        }
     }
 }
