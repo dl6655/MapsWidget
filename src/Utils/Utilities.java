b66495af2c5f8864be5039f4445c50dff63d3c26
Ding Li <dingli@le.com>
2017-04-19 17:51:58 +0800

    LeccoMapsWidget: Google Maps notification data display in widget
    Google Maps notification data synchronization display on widget
    
    Ticket: IOFV-5567
    
    Change-Id: I06532f0e0d62ca817df086bb3d670de1a112736f
    Signed-off-by: Ding Li <dingli@le.com>

---
+++ src/Utils/Utilities.java
@@ -0,0 +1,7 @@
+package Utils;
+
+/**
+ * Created by root on 17-4-18.
+ */
+public class Utilities {
+}
--- src/com/leeco/LeecoMapsWidget/MapsWidgetProvider.java
+++ src/com/leeco/LeecoMapsWidget/MapsWidgetProvider.java
@@ -9,23 +9,32 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Icon;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.os.RemoteException;
 import android.service.notification.StatusBarNotification;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by dingli on 17-3-23.
  */
 public class MapsWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "MapsWidgetProvider";
-    private Context mContext;
+    private static Context mContext;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-        defaultAppWidget(context, appWidgetIds);
+        getShared();
         final NotificationManager nmr = context.getSystemService(NotificationManager.class);
         nmr.registerNotificationInterceptListener(mNotificationListener);
         mContext = context;
@@ -35,21 +44,6 @@ public class MapsWidgetProvider extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 //        receive(context,intent);
-    }
-
-    private void defaultAppWidget(Context context, int[] appWidgetIds) {
-        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.maps_appwidget);
-        linkButtons(context, views, false /* not playing */);
-        pushUpdate(context, appWidgetIds, views);
-    }
-
-    private void pushUpdate(Context context, int[] appWidgetIds, RemoteViews views) {
-        final AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-        if (appWidgetIds != null) {
-            appWidgetManager.updateAppWidget(appWidgetIds, views);
-        } else {
-            appWidgetManager.updateAppWidget(new ComponentName(context, this.getClass()), views);
-        }
     }
 
     private void linkButtons(Context context, RemoteViews views, boolean playerActive) {
@@ -74,12 +68,10 @@ public class MapsWidgetProvider extends AppWidgetProvider {
         }
         String pkg = notification.getPackageName();
         if (pkg.equals("com.leeco.notificationTestData") || pkg.equals("com.google.android.apps.maps")) {
-
             Notification n = notification.getNotification();
             if (n == null) {
                 return;
             }
-
             fillReceiveData(context, n, action);
         }
 
@@ -102,6 +94,7 @@ public class MapsWidgetProvider extends AppWidgetProvider {
 
         }
     }
+
 
     /**
      * Implementation of {@link android.app.IInterceptNotification} to listen notification changes from
@@ -127,20 +120,123 @@ public class MapsWidgetProvider extends AppWidgetProvider {
         }
     };
 
-    private void fillWidgetData(Notification n) {
-        String title = n.extras.getString(Notification.EXTRA_TITLE);
-        String text = n.extras.getString(Notification.EXTRA_TEXT);
-        Icon iconSmall = n.getSmallIcon();
-        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
-        final RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.maps_appwidget);
-        views.setViewVisibility(R.id.maps_background, View.GONE);
-        views.setViewVisibility(R.id.maps_linear, View.VISIBLE);
-        views.setViewVisibility(R.id.maps_icon, View.VISIBLE);
+    private void fillHandlerData(Context context, String title0, String title1) {
+        String title = title0;
+        String text = title1;
+//        Icon iconSmall = n.getSmallIcon();
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.maps_appwidget);
+        linkButtons(context, views, false /* not playing */);
         if (title != null) {
             views.setTextViewText(R.id.maps_title, title);
             views.setTextViewText(R.id.maps_text, text);
-            views.setImageViewIcon(R.id.maps_icon, iconSmall);
-            appWidgetManager.updateAppWidget(new ComponentName(mContext, MapsWidgetProvider.class), views);
+
         }
+        appWidgetManager.updateAppWidget(new ComponentName(context, MapsWidgetProvider.class), views);
+    }
+
+    private void fillWidgetData(Notification n) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
+        RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.maps_appwidget);
+        views = n.contentView;
+
+        ViewGroup viewGroup = (ViewGroup) views.apply(mContext, null);
+        ArrayList<String> strArrayList = new ArrayList<String>();
+
+        int childSize = viewGroup.getChildCount();
+        for (int i = 0; i < childSize; i++) {
+            View view1 = viewGroup.getChildAt(i);
+            if (i == 0) {
+                ImageView imageView = (ImageView) view1;
+            }
+            if (i == 1) {
+                ViewGroup viewGroup1 = (ViewGroup) view1;
+                int childSize1 = viewGroup1.getChildCount();
+                if (childSize1 > 0) {
+                    for (int i1 = 0; i1 < childSize1; i1++) {
+                        ViewGroup viewGroup2 = (ViewGroup) viewGroup1.getChildAt(i1);
+                        int childSize2 = viewGroup2.getChildCount();
+                        if (childSize2 > 0) {
+                            for (int i2 = 0; i2 < childSize2; i2++) {
+                                View view3 = viewGroup2.getChildAt(i2);
+                                TextView textView = (TextView) view3;
+                                String str = textView.getText().toString();
+                                strArrayList.add(str);
+                            }
+                            mHandler.obtainMessage(MSG_SETWIDGETDATA, strArrayList).sendToTarget();
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    private ArrayList<ViewGroup> viewArrayList = new ArrayList<ViewGroup>();
+
+    private ArrayList<ViewGroup> getView(ViewGroup viewGroup) {
+        int childSize = viewGroup.getChildCount();
+        if (childSize > 0) {
+            for (int i = 0; i < childSize; i++) {
+                ViewGroup viewGroup1 = (ViewGroup) (viewGroup.getChildAt(i));
+                if (viewGroup1.getChildCount() > 0) {
+                    getView(viewGroup1);
+                } else {
+                    viewArrayList = new ArrayList<ViewGroup>();
+                    viewArrayList.add(viewGroup1);
+                }
+            }
+        }
+        return viewArrayList;
+    }
+
+    private static final int MSG_SETWIDGETDATA = 1, MSG_GETWIDGETDATA = 2;
+    private final Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_SETWIDGETDATA:
+                    setShared((ArrayList<String>) msg.obj);
+                    break;
+                case MSG_GETWIDGETDATA:
+                    getShared();
+                    break;
+            }
+        }
+    };
+
+    public void setShared(final ArrayList<String> strs) {
+        String title0 = "";
+        String title1 = "";
+        SharedPreferences sp = mContext.getSharedPreferences("googleMapNotificationData", mContext.MODE_APPEND);
+        SharedPreferences.Editor editor = sp.edit();
+        for (int i = 0; i < strs.size(); i++) {
+            String str = strs.get(i);
+            if (str != null && str.length() > 0) {
+                editor.putString("title" + i, str);
+                editor.commit();
+                if (i == 0) {
+                    title0 = str;
+                }
+                if (i == 1) {
+                    title1 = str;
+                }
+            }
+        }
+        fillHandlerData(mContext, title0, title1);
+    }
+
+    private void getShared() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sp = mContext.getSharedPreferences("googleMapNotificationData", mContext.MODE_APPEND);
+                String title0 = sp.getString("title0", mContext.getResources().getString(R.string.city_name));
+                String title1 = sp.getString("title1", mContext.getResources().getString(R.string.street_name));
+                fillHandlerData(mContext, title0, title1);
+            }
+        });
+
     }
 }
